893cf3e2addc830dd851bc0c99d81efd2e28e779
Eric Stallings <stallings.eric@gmail.com>
2020-01-04 14:14:58 -0800

    Initial Commit: File Structure.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }, 
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }, {
+      test: /\.s?css$/,
+      exclude: /node_modules/,
+      use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader'},
+        {loader: 'sass-loader'}
+      ] 
+    }]
+  },
+  devServer: {
+    publicPath: path.resolve(__dirname, '/build/'),
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000'
+    },
+    hot: true,
+  },
+  mode: process.env.NODE_ENV
+}
